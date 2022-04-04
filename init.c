29b7870b5e4c281b43c58ab8eb339e225def1e8b
Millie <Millie@MacBook-Pro-Kamilla.local>
2022-04-05 00:21:32 +0300

    сегодня поздравили свету с др который прошел пару (?) недель назад

--- init.c
+++ init.c
@@ -8,4 +8,5 @@ void	distribution(t_struct *game)
 	game->wall.path, &game->wall.width, &game->wall.height);
 	game->floor.img = mlx_xpm_file_to_image(game->mlx, \
 	game->floor.path, &game->floor.width, &game->floor.height);
+	//no positions
 }
